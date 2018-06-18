ae77aa2fcbbd718f6680dc97c06fc1795c01b73e
Megly <ec2-user@ip-172-31-11-140.us-east-2.compute.internal>
2018-06-18 04:46:19 +0000

    Have function

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -13,6 +13,9 @@
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
                                 @endif
+                                @if (Auth::check())
+                                    @include('items.have_button', ['item' => $item])
+                                @endif
                             </div>
                         </div>
                     </div>
